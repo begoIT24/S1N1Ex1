aeffd1e6d369838a8885204a15af43854392bfec
begoIT24 <blopezprof@gmail.com>
2024-01-15 11:38:36 +0100

    Tema 2 Exercicis 1 i 2

---
+++ Tema2/nivel-1/Ex1.php
@@ -0,0 +1,11 @@
+<?php
+$int = 2;
+$double = 1.234;
+$string = "Hello world";
+$boolean = true;
+
+echo $int . "<br/>";
+echo $double . "<br/>" ;
+echo $string . "<br/>";
+echo $boolean;
+?>
---
+++ Tema2/nivel-1/Ex2.php
@@ -0,0 +1,16 @@
+<?php
+$hello = "Hello, world!";
+$curs = "Aquest és el curs de php";
+
+echo $hello . "<br/>";
+
+echo strlen($hello) . "<br/>";
+
+$hello1 = strtoupper($hello);
+echo $hello1 . "<br/>";
+
+$hello2 = strrev($hello);
+echo $hello2 . "<br/>";;
+
+echo "$hello  $curs";
+?>
